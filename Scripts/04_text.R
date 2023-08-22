22c43a69823de199ea3f3aade47516e0e991249d
hoda maz <adohmk@gmail.com>
2023-08-22 21:45:18 +0200

    updated text script

--- Scripts/04_text.R
+++ Scripts/04_text.R
@@ -31,9 +31,10 @@ bikes_tbl %>%
                as.numeric()) %>% 
     mutate(black = model %>% str_detect("Black") %>%  as.numeric())  
     
-bikes_tbl %>% 
-  select(model) %>% 
-  mutate(red = model %>% str_detect(pattern = 'Red') %>% as.numeric()) %>% view()
+# bikes_tbl %>% 
+#   select(model) %>% 
+#   mutate(red = model %>% str_detect(pattern = 'Red') %>% as.numeric()) %>% view()
+
 # 1.2 Case & Concatenation ----
 
 
@@ -65,12 +66,11 @@ bike_orderlines_tbl %>%
 
 # Vector
 c("Road - Elite Road - Carbon", "Road - Elite Road") %>% 
-    str_split(pattern = " - ", simplify = TRUE)
+    str_split(pattern = " - ", simplify = TRUE) # simplify returns a matrix, with empty strings when no value is given
 
 # Tibble
 bikes_tbl %>% 
-    select(description) %>% 
-    separate(col    = description, 
+    separate(col    = description, # from tidyr
              into   = c("category_1", "category_2", "category_3"),
              sep    = " - ",
              remove = FALSE)
@@ -78,12 +78,12 @@ bikes_tbl %>%
 
 # 1.4 Trimming Text ----
 
-" text with space   " %>% str_trim(side = "right") # "both" and "left"
+" text with space   " %>% str_trim(side = "both") # "both" and "left"
 
 # 1.5 Replacement: Used with mutate() [and optionally case_when()] ----
 
 # Vector
-c("CAAD12", "CAAD", "CAAD8") %>% str_replace(pattern = "[0-9]", replacement = "")
+c("CAAD12", "CAAD", "CAAD8") %>% str_replace(pattern = "[0-9]", replacement = "") # finds the FIRST pattern
 # one number was retained, it only looks for one instance
 c("CAAD12", "CAAD", "CAAD8") %>% str_replace_all(pattern = "[0-9]", replacement = "X")
 
@@ -99,9 +99,10 @@ bikes_tbl %>%
 # 1.6 Formatting Numbers ----
 # 
 value <- 1e6
+(value) %>% scales::number(prefix = "$", suffix = "M")
 (value / 1e6) %>% scales::number(prefix = "$", suffix = "M")
 value %>% scales::number(prefix = "$", big.mark = ",")
-value %>% scales::dollar(scale = 1/1e6, suffix = "M")
+value %>% scales::dollar(scale = 1/1e6, suffix = "M") # scale value will be multiplied
 
 pct <- 0.15
 pct %>% scales::number(scale = 100, suffix = "%")
@@ -115,20 +116,47 @@ bike_orderlines_tbl %>%
 
 # Appending text to column names
 bike_orderlines_tbl %>% 
-    set_names(str_glue("{names(.)}_bike"))
+    set_names(str_glue("{names(.)}_bike")) # from purrr
 
 # Appending text to specific column names
 bike_orderlines_columns_tbl <- 
     bike_orderlines_tbl %>% 
-    rename_at(.vars = vars(model:frame_material), # select from model to frame_material
+    rename_at(.vars = vars(model:frame_material), # select from model to frame_material/ from dplyr
               .funs = ~ str_c("prod_", .)) %>% 
     rename_at(.vars = vars(bikeshop_name:state),
-              ~ str_c("cust_", .)) 
+              ~ str_c("cust_", .)) # pass anonymouse function
 
 bike_orderlines_columns_tbl
 
 bike_orderlines_columns_tbl %>% 
-    select(contains("cust_"))
-# 2.0 Feature Engineering with Text -----
+    select(contains("cust_"), total_price, price)
+# 2.0 Feature Engineering with Text ----- (real world experience)
 # Investigating "model" and extracting well-formatted features
+
+bikes_tbl %>% 
+  select(model) %>% 
+  #fix a typo
+  mutate(model = case_when(
+    model == 'CAAD Disc Ultegra' ~ 'CAAD12 Disc Ultegra',
+    TRUE ~ model
+  )) %>% 
+  view()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
